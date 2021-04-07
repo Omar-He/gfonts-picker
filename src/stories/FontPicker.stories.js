fe78c5d3beacaba8794c4bd306b980684e379bad
Omar Hendi <omar.hendi92@gmail.com>
2021-04-07 17:25:54 +0300

    add storybook

---
+++ src/stories/FontPicker.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Main from "../components/main"
+import { storiesOf } from "@storybook/react";
+
+storiesOf("G Fonts picker / List", module)
+  .add("Default", () => <Main/>);
+
+
